08ac759d8fa095af8caff31ad46e4557e2d3cab3
abdessalem <belhadj.abdessalem@gmail.com>
2014-11-28 22:43:18 +0100

    premerge

--- src/main/java/com/webcaisse/dao/hibernate/impl/ClientDao.java
+++ src/main/java/com/webcaisse/dao/hibernate/impl/ClientDao.java
@@ -10,20 +10,19 @@ import org.springframework.transaction.annotation.Transactional;
 import com.webcaisse.dao.hibernate.IClient;
 import com.webcaisse.dao.hibernate.model.Client;
 
-
 public class ClientDao implements IClient {
 
-	
-	@Resource(name="sessionFactory")
+	@Resource(name = "sessionFactory")
 	SessionFactory sessionFactory;
-	
+
 	@Transactional
 	public List<Client> rechercherClient(Long idSociete) {
-		
-			 Query req = sessionFactory.getCurrentSession().createQuery("select c from Client c where c.societe.id=:x " ) ;
-			    req.setParameter("x", idSociete) ;
-			    return req.list() ;
-		
+
+		Query req = sessionFactory.getCurrentSession().createQuery(
+				"select c from Client c where c.societe.id=:x ");
+		req.setParameter("x", idSociete);
+		return req.list();
+
 	}
 
 }
