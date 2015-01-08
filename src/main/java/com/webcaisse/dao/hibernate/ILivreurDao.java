7604507151920b9cd3451c63ed642f8263ff6a91
khaled <sghaierkhaled@live.com>
2015-01-08 20:30:52 +0100

    dev param livreur

---
+++ src/main/java/com/webcaisse/dao/hibernate/ILivreurDao.java
@@ -0,0 +1,14 @@
+package com.webcaisse.dao.hibernate;
+
+import java.util.List;
+
+import com.webcaisse.dao.hibernate.model.Livreur;
+import com.webcaisse.dao.hibernate.model.User;
+
+
+public interface ILivreurDao {
+	
+	void sauvegarderLivreur (Livreur livreur) ;
+	public  List<Livreur> rechercherLivreur(Long idSociete) ;
+
+}
---
+++ src/main/java/com/webcaisse/dao/hibernate/impl/LivreurDao.java
@@ -0,0 +1,37 @@
+package com.webcaisse.dao.hibernate.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.webcaisse.dao.hibernate.ILivreurDao;
+import com.webcaisse.dao.hibernate.model.Livreur;
+import org.hibernate.Query;
+
+public class LivreurDao implements ILivreurDao{
+
+	
+	@Resource(name = "sessionFactory")
+	SessionFactory sessionFactory;
+	
+	
+	@Transactional
+	public void sauvegarderLivreur(Livreur livreur) {
+		
+		sessionFactory.getCurrentSession().save(livreur) ;
+		
+	}
+
+	@Transactional
+	public List<Livreur> rechercherLivreur(Long idSociete) {
+		
+		Query query= sessionFactory.getCurrentSession().createQuery(" select l from Livreur l where l.societe.id=:x ") ;
+		query.setParameter("x", idSociete) ;
+		
+		return query.list();
+	}
+
+}
