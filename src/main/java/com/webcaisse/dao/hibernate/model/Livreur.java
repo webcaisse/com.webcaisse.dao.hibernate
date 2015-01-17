88b89794352058da55aeb315725a512e90cfeada
abdessalem <belhadj.abdessalem@gmail.com>
2015-01-17 22:36:45 +0100

    premerge

--- src/main/java/com/webcaisse/dao/hibernate/model/Livreur.java
+++ src/main/java/com/webcaisse/dao/hibernate/model/Livreur.java
@@ -1,16 +1,11 @@
 package com.webcaisse.dao.hibernate.model;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
